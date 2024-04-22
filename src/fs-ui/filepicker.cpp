d0bbe76fdaf18eb8457ce053299f791646e5de50
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2024-04-22 22:25:46 +0200

    Move file button next to file input field

--- src/fs-ui/filepicker.cpp
+++ src/fs-ui/filepicker.cpp
@@ -1,15 +1,25 @@
 
+#define IMGUI_DEFINE_MATH_OPERATORS 1
 #include "imgui_internal.h"
+
 #include "fs-ui/filepicker.hpp"
 
-bool FsUi::Filepicker(const char *label, char *buf, size_t buf_size, ImGuiInputTextFlags flags, ImGuiInputTextCallback callback, void* user_data)
+bool FsUi::Filepicker(const char *label, char *buf, size_t buf_size, ImGuiInputTextFlags flags, ImGuiInputTextCallback callback, void* callback_user_data)
 {
-    bool text_edited = ImGui::InputTextEx(label, NULL, buf, (int)buf_size, ImVec2(0, 0), flags, callback, user_data);
+    ImGuiStyle &st = ImGui::GetStyle();
+    ImGui::PushID(label);
+    bool text_edited = ImGui::InputTextEx("", NULL, buf, (int)buf_size, ImVec2(0, 0), flags, callback, callback_user_data);
+    ImGui::SameLine();
+    ImGui::Dummy({-st.FramePadding.x * 2, 0});
     ImGui::SameLine();
     bool picker_clicked = ImGui::Button("...");
+    ImGui::SameLine();
+    ImGui::Text(label);
 
     if (picker_clicked)
     {}
+
+    ImGui::PopID();
 
     return text_edited;
 }
