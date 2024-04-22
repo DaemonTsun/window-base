66af14b63e362cb721229532bb848c3cd38b97d3
DaemonTsun <96687758+DaemonTsun@users.noreply.github.com>
2024-04-22 17:44:14 +0200

    Fix find-font in template

--- src/window/find_font.hpp
+++ src/window/find_font.hpp
@@ -7,15 +7,20 @@ extern "C"
 {
 struct ff_cache;
 
+// See shl/allocator.hpp for how an allocator looks like.
+// If allocator is nullptr, uses a default allocator.
 ff_cache *ff_load_font_cache(void *allocator = nullptr);
 void      ff_unload_font_cache(ff_cache *cache);
 
 // Tries to find a font by name and style, and returns the path to the font file.
 // font_name and style_name must be _exact_.
+// style_name may be nullptr or an empty string for the default style.
 const char *ff_find_font_path(ff_cache *cache, const char *font_name, const char *style_name);
 
 // Much slower, but easier to use.
 // Names are checked using "begins_with", i.e. only start must match.
+// NOTE: if there's multiple matches, it is not guaranteed which font path is
+// returned. Use ff_find_font_path instead if you need an exact match.
 const char *ff_find_font_path_vague(ff_cache *cache, const char *font_name, const char *style_name);
 
 
@@ -31,5 +36,7 @@ const char *ff_find_font_path_vague(ff_cache *cache, const char *font_name, cons
 
 // names and styles must be exact
 const char *ff_find_first_font_path(ff_cache *cache, const char **font_names_and_styles, int count, int *found_index);
+
+// names don't have to be exact here
 const char *ff_find_first_font_path_vague(ff_cache *cache, const char **font_names_and_styles, int count, int *found_index);
 }
